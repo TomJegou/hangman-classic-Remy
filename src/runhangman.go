24bf64413a9aa2935b5383cb764b90d0cc3f517e
TomJegou <tomjegou10@gmail.com>
2022-11-24 16:50:33 +0100

    implementing quiting in main loop of hangman

--- src/runhangman.go
+++ src/runhangman.go
@@ -37,20 +37,49 @@ out:
 	attempt = 10
 	attemptChan <- attempt
 	fmt.Println("bienvenu dans la boucle")
-	for content := range inputChan {
-		fmt.Println("Début de la boucle de hang")
-		letter := StartGame(wordDash, usedLetters, usedWords, content)
-		fmt.Println(letter)
-		if !CheckUsedLetter(usedLetters, letter) {
-			if letter != "" {
-				fmt.Println("\033[31mYou already try it once !\033[0m")
-			}
-		} else if len(letter) > 1 {
-			if letter == word {
-				responseChan <- "50536101b1c465eafbecc8fca26eeb18a2ac8a2f83570bade315c5a112363cdfd820acad2ab234f91d43f0db8fed0cec400a1109ad8f99c21b5b74f59e8bb00d"
+	for {
+		select {
+		default:
+			continue
+		case <-quitChan:
+			fmt.Println("Quiting Hangman")
+			return
+		case input := <-inputChan:
+			fmt.Println("Début de la boucle de hang")
+			letter := StartGame(wordDash, usedLetters, usedWords, input)
+			fmt.Println(letter)
+			if !CheckUsedLetter(usedLetters, letter) {
+				if letter != "" {
+					fmt.Println("\033[31mYou already try it once !\033[0m")
+				}
+			} else if len(letter) > 1 {
+				if letter == word {
+					responseChan <- "50536101b1c465eafbecc8fca26eeb18a2ac8a2f83570bade315c5a112363cdfd820acad2ab234f91d43f0db8fed0cec400a1109ad8f99c21b5b74f59e8bb00d"
+					attemptChan <- attempt
+					break
+				} else {
+					attempt--
+					Display(attempt)
+					if attempt == 0 {
+						responseChan <- "889ce65f137b3b9aa1005f417d7972c948b8bb6360cbdd4118cb05a29d37905744fc0dbc3d17c1de02689d837bfea5bb8114a994f9c1a53dddb993139ab2974c"
+						attemptChan <- attempt
+						wordChan <- word
+						break
+					}
+				}
+			} else if CheckLetterInWord(word, letter) {
+				wordDash = Replace(word, wordDash, letter)
+				if wordDash == word {
+					responseChan <- "50536101b1c465eafbecc8fca26eeb18a2ac8a2f83570bade315c5a112363cdfd820acad2ab234f91d43f0db8fed0cec400a1109ad8f99c21b5b74f59e8bb00d"
+					attemptChan <- attempt
+					break
+				}
+			} else if letter == "" {
+				responseChan <- wordDash
 				attemptChan <- attempt
-				break
+				continue
 			} else {
+				usedLetters += letter
 				attempt--
 				Display(attempt)
 				if attempt == 0 {
@@ -60,30 +89,9 @@ out:
 					break
 				}
 			}
-		} else if CheckLetterInWord(word, letter) {
-			wordDash = Replace(word, wordDash, letter)
-			if wordDash == word {
-				responseChan <- "50536101b1c465eafbecc8fca26eeb18a2ac8a2f83570bade315c5a112363cdfd820acad2ab234f91d43f0db8fed0cec400a1109ad8f99c21b5b74f59e8bb00d"
-				attemptChan <- attempt
-				break
-			}
-		} else if letter == "" {
 			responseChan <- wordDash
 			attemptChan <- attempt
-			continue
-		} else {
-			usedLetters += letter
-			attempt--
-			Display(attempt)
-			if attempt == 0 {
-				responseChan <- "889ce65f137b3b9aa1005f417d7972c948b8bb6360cbdd4118cb05a29d37905744fc0dbc3d17c1de02689d837bfea5bb8114a994f9c1a53dddb993139ab2974c"
-				attemptChan <- attempt
-				wordChan <- word
-				break
-			}
+			fmt.Println("Fin de l'envoie")
 		}
-		responseChan <- wordDash
-		attemptChan <- attempt
-		fmt.Println("Fin de l'envoie")
 	}
 }
